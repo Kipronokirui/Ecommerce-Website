e9461df0b7a9075e4bf3360e37b909d58586e367
Kipronokirui <victorkipronokirui2@gmail.com>
2024-01-09 23:11:20 +0300

    Redux Persist

--- store/store.js
+++ store/store.js
@@ -2,13 +2,51 @@ import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 import {cartSlice} from "./features/cartSlice";
 import { composeWithDevTools } from '@redux-devtools/extension';
+// import storage from 'redux-persist/lib/storage';
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+
+const createNoopStorage = () => {
+    return {
+      getItem(_key) {
+        return Promise.resolve(null);
+      },
+      setItem(_key, value) {
+        return Promise.resolve(value);
+      },
+      removeItem(_key) {
+        return Promise.resolve();
+      },
+    };
+};
+const storage = typeof window !== "undefined" ? createWebStorage("local") : createNoopStorage();
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
 
 export const store = configureStore({
     reducer:{
-        cart:cartSlice.reducer
+        cart:persistReducer(persistConfig, cartSlice.reducer)
     },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
 
+export const persistor = persistStore(store)
 // export const RootState = store.getState
 // export const AppDispatch = store.dispatch
 
